899ad0ebd89c15bc96b262e8121304d63a3fdd3b
Dirk Baeumer <dirkb@microsoft.com>
2020-03-05 12:56:17 +0100

    Update dependencies

--- samples/typescript/index.ts
+++ samples/typescript/index.ts
@@ -1,43 +1,11 @@
-import { IComputedValue } from 'mobx';
+import { IDisposable } from 'mobx';
 
-let x: IComputedValue<number>;
-console.log(x);
+class A implements IDisposable {
+	public dispose(): void {
+	}
+}
 
-
-// const map: Map<string, string> = new Map();
-// map.set('dirk', 'baeumer');
-
-// interface A {
-// 	x: {
-// 		y: string;
-// 	}
-// }
-
-// interface B {
-// 	x: {
-// 		y: number;
-// 	}
-// }
-
-// let z: A | B;
-
-// let v = z.x.y;
-// console.log(v);
-
-// import { mul } from './sub/provide';
-
-
-// const result: number = mul(10, 20);
-// if (result !== 200) {
-// 	throw new Error(`Multiplication failed`);
-// }
-
-
-// export class MyClass {
-// 	constructor() {
-// 	}
-
-// 	public mul(a: number, b: number): number {
-// 		return a * b;
-// 	}
-// }
+class B implements IDisposable {
+	public dispose(): void {
+	}
+}
