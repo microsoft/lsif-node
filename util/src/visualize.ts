d824f5398f43a0d92b35c352c9c2c5db3070e038
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2020-10-29 13:49:49 +0000

    lsif-util: Fix #70 by replacing \\" with \" in vertex extra info sections (#112)
    * lsif-util: Fixed #70 by replacing \\" with \" in vertex extra info sections
    
    * /re/ -> new RegExp

--- util/src/visualize.ts
+++ util/src/visualize.ts
@@ -75,7 +75,9 @@ function printDOT(edges: { [id: string]: LSIF.Element }, vertices: { [id: string
 		Object.keys(extraInfo).forEach((property: string) => {
 			const value: string = JSON.stringify((extraInfo as any)[property]);
 			const re: RegExp = new RegExp('"', 'g');
-			extraText += `\n${property} = ${value.replace(re, '\\"')}`;
+      extraText += `\n${property} = ${value.replace(re, '\\"')}`;
+      const reEscaped: RegExp = new RegExp('\\\\\\\\"', 'g')
+      extraText = extraText.replace(reEscaped, '\\"');
 		});
 
 		digraph += `  ${vertex.id} [label="[${vertex.id}] ${vertex.label}${extraText}"]\n`;
