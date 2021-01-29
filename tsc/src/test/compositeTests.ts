d0e2cce621684a946c537695da5d8979ba397c0a
Dirk Baeumer <dirkb@microsoft.com>
2021-01-29 17:58:41 +0100

    Fix union tests

--- tsc/src/test/compositeTests.ts
+++ tsc/src/test/compositeTests.ts
@@ -53,13 +53,14 @@ suite('Union Types', () => {
 				].join(os.EOL)
 			]
 		]), compilerOptions);
+		assert.deepEqual(emitter.lastId, 158);
 		const validate: Element[] = [
-			JSON.parse('{"id":132,"type":"vertex","label":"resultSet"}'),
-			JSON.parse('{"id":133,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":[a:A.name,a:B.name]","unique":"group","kind":"export"}'),
-			JSON.parse('{"id":160,"type":"vertex","label":"referenceResult"}'),
-			JSON.parse('{"id":161,"type":"edge","label":"textDocument/references","outV":132,"inV":160}'),
-			JSON.parse('{"id":162,"type":"edge","label":"item","outV":160,"inVs":[70,83],"shard":2,"property":"referenceResults"}'),
-			JSON.parse('{"id":163,"type":"edge","label":"item","outV":160,"inVs":[23,37],"shard":2,"property":"referenceLinks"}')
+			JSON.parse('{"id":128,"type":"vertex","label":"resultSet"}'),
+			JSON.parse('{"id":129,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":[a:A.name,a:B.name]","unique":"group","kind":"export"}'),
+			JSON.parse('{"id":130,"type":"edge","label":"moniker","outV":128,"inV":129}'),
+			JSON.parse('{"id":155,"type":"edge","label":"textDocument/references","outV":128,"inV":154}'),
+			JSON.parse('{"id":156,"type":"edge","label":"item","outV":154,"inVs":[70,83],"shard":2,"property":"referenceResults"}'),
+			JSON.parse('{"id":157,"type":"edge","label":"item","outV":154,"inVs":[23,37],"shard":2,"property":"referenceLinks"}')
 		];
 		for (const elem of validate) {
 			assert.deepEqual(emitter.elements.get(elem.id), elem);
@@ -85,15 +86,17 @@ suite('Union Types', () => {
 				].join(os.EOL)
 			]
 		]), compilerOptions);
+		assert.deepEqual(emitter.lastId, 186);
 		const validate: Element[] = [
 			JSON.parse('{"id":23,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:A.name","unique":"group","kind":"export"}'),
 			JSON.parse('{"id":37,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:B.name","unique":"group","kind":"export"}'),
-			JSON.parse('{"id":160,"type":"vertex","label":"resultSet"}'),
-			JSON.parse('{"id":161,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":[a:A.name,a:B.name]","unique":"group","kind":"export"}'),
-			JSON.parse('{"id":188,"type":"vertex","label":"referenceResult"}'),
-			JSON.parse('{"id":189,"type":"edge","label":"textDocument/references","outV":160,"inV":188}'),
-			JSON.parse('{"id":190,"type":"edge","label":"item","outV":188,"inVs":[86,99],"shard":2,"property":"referenceResults"}'),
-			JSON.parse('{"id":191,"type":"edge","label":"item","outV":188,"inVs":[23,37],"shard":2,"property":"referenceLinks"}')
+			JSON.parse('{"id":156,"type":"vertex","label":"resultSet"}'),
+			JSON.parse('{"id":157,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":[a:A.name,a:B.name]","unique":"group","kind":"export"}'),
+			JSON.parse('{"id":158,"type":"edge","label":"moniker","outV":156,"inV":157}'),
+			JSON.parse('{"id":182,"type":"vertex","label":"referenceResult"}'),
+			JSON.parse('{"id":183,"type":"edge","label":"textDocument/references","outV":156,"inV":182}'),
+			JSON.parse('{"id":184,"type":"edge","label":"item","outV":182,"inVs":[86,99],"shard":2,"property":"referenceResults"}'),
+			JSON.parse('{"id":185,"type":"edge","label":"item","outV":182,"inVs":[23,37],"shard":2,"property":"referenceLinks"}'),
 		];
 		for (const elem of validate) {
 			assert.deepEqual(emitter.elements.get(elem.id), elem);
@@ -124,13 +127,13 @@ suite('Union Types', () => {
 				].join(os.EOL)
 			]
 		]), compilerOptions);
-		assert.deepEqual(emitter.lastId, 195);
+		assert.deepEqual(emitter.lastId, 177);
 		const validate: Element[] = [
-			JSON.parse('{"id":42,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:D.name","unique":"group","kind":"export"}'),
-			JSON.parse('{"id":43,"type":"edge","label":"attach","outV":42,"inV":26}'),
-			JSON.parse('{"id":44,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:D.name","unique":"group","kind":"export"}'),
-			JSON.parse('{"id":45,"type":"edge","label":"attach","outV":44,"inV":36}'),
-			JSON.parse('{"id":109,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":a:D.name","unique":"group","kind":"export"}')
+			JSON.parse('{"id":36,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:D.name","unique":"group","kind":"export"}'),
+			JSON.parse('{"id":37,"type":"edge","label":"attach","outV":36,"inV":23}'),
+			JSON.parse('{"id":38,"type":"vertex","label":"moniker","scheme":"tsc","identifier":"a:D.name","unique":"group","kind":"export"}'),
+			JSON.parse('{"id":39,"type":"edge","label":"attach","outV":38,"inV":30}'),
+			JSON.parse('{"id":99,"type":"vertex","label":"moniker","scheme":"tsc","identifier":":a:D.name","unique":"group","kind":"export"}')
 		];
 		for (const elem of validate) {
 			assert.deepEqual(emitter.elements.get(elem.id), elem);
