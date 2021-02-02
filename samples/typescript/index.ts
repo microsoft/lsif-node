7ecd86bea3263f24a251f7578b16c98b16221cfb
Dirk Baeumer <dirkb@microsoft.com>
2021-02-02 10:56:57 +0100

    Update samples

--- samples/typescript/index.ts
+++ samples/typescript/index.ts
@@ -1,18 +1,18 @@
-namespace N {
-	export function foo(): void { }
-	function bar(): void { }
-}
-
-export class C {
-
-	public static C(): void { }
-
-	public foo(): void { }
-	private bar(): void { }
-}
-
-// export interface I {
-// 	foo(): void;
-// 	"123": number;
-// 	__bar: string;
+// namespace N {
+// 	export function foo(): void { }
+// 	function bar(): void { }
 // }
+
+				// export interface I {
+				// 	// field: { key: number; }
+				// 	foo(): { key: number };
+				// 	// "123": number;
+				// 	// __bar: string;
+				// }
+
+// interface Func {
+// 	(arg: number): { key: number };
+// 	arg: string;
+// }
+
+export * from './provide';
--- samples/typescript/provide.ts
+++ samples/typescript/provide.ts
@@ -3,12 +3,22 @@
 
 // export { _foo as foo, _foo as foo2 };
 
-interface Base {
-	foo(): void;
+// interface Base {
+// 	foo(): void;
+// }
+
+// export interface RAL extends Base {
+// 	readonly console: {
+// 		error(message?: any, ...optionalParams: any[]): void;
+// 	}
+// }
+
+// export default function (): void {
+// }
+
+export function foo(): void {
 }
 
-export interface RAL extends Base {
-	readonly console: {
-		error(message?: any, ...optionalParams: any[]): void;
-	}
+export function bar(): { value: number } {
+	return { value: 10 };
 }
