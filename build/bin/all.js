b985fdbdfe559b09ab1a5bd713a6685f7b57a1e4
Dirk Baeumer <dirkb@microsoft.com>
2021-02-11 16:31:08 +0100

    :lipstick:

--- build/bin/all.js
+++ build/bin/all.js
@@ -3,7 +3,9 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+"use strict";
 
+//@ts-check
 const path = require('path');
 const child_process = require('child_process')
 
