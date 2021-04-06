edc759465e55d448d7504ea0f84d1771074824b5
Dirk Baeumer <dirkb@microsoft.com>
2021-04-06 13:44:49 +0200

    Remove validate from util package and point to tooling.

--- util/src/main.ts
+++ util/src/main.ts
@@ -7,7 +7,6 @@ import * as LSIF from 'lsif-protocol';
 import * as readline from 'readline';
 import * as yargs from 'yargs';
 import { getFilteredIds, IFilter } from './filter';
-import { validate } from './validate';
 import { visualize } from './visualize';
 
 function readInput(format: string, inputPath: string, callback: (input: LSIF.Element[]) => void): void {
@@ -69,20 +68,9 @@ export function main(): void {
 					describe: 'Path to input file or --stdin',
 					type: 'string',
 				},
-			}, (argv: yargs.Arguments) => {
-				if (!argv.stdin && argv.file === undefined) {
-					yargs.showHelp('log');
-					console.error('\nError: Missing input file. Did you forget --stdin?');
-					process.exitCode = 1;
-				} else {
-					readInput(
-						argv.inputFormat as string,
-						argv.stdin ? '--stdin' : argv.file as string,
-						(input: LSIF.Element[]) => {
-							const filter: IFilter = argv as unknown as IFilter;
-							process.exitCode = validate(input, getFilteredIds(filter, input));
-						});
-				}
+			}, () => {
+				console.error(`The validate command got removed. Please use lsif validate instead.`);
+				process.exitCode = -1;
 			})
 
 		// Visualization tool
