30365b74e9683104f15c34b2dc253768c159c220
Dirk Baeumer <dirkb@microsoft.com>
2021-04-21 14:30:03 +0200

    Fix util test

--- util/src/test/main.ts
+++ util/src/test/main.ts
@@ -20,9 +20,4 @@ describe('The console-line interface usage', () => {
 		main();
 		expect(process.exitCode).toBe(1);
 	});
-	it('Should ask for an input file if none specified and --stdin is missing', () => {
-		yargs.parse(['validate']);
-		main();
-		expect(process.exitCode).toBe(1);
-	});
 });
