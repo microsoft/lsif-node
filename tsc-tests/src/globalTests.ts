2537140871ce9e4e9c1fa27332d044d2c5ea1bea
Dirk Baeumer <dirkb@microsoft.com>
2021-04-23 16:07:46 +0200

    Fix compile errors

--- tsc-tests/src/globalTests.ts
+++ tsc-tests/src/globalTests.ts
@@ -7,7 +7,7 @@ import * as assert from 'assert';
 import * as os from 'os';
 
 import { lsif, assertElement } from './lsifs';
-import { Element, VertexLabels, Vertex, Edge } from 'lsif-protocol';
+import { Element, Vertex, Edge } from 'lsif-protocol';
 
 suite('Global Module Tests', () => {
 	test('Single export', async () => {
