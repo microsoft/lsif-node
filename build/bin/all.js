8e581b3bb175c0c2a3f2ba3285174640468fc610
Dirk Baeumer <dirkb@microsoft.com>
2022-01-06 11:27:02 +0100

    Remove util package

--- build/bin/all.js
+++ build/bin/all.js
@@ -12,7 +12,7 @@ const child_process = require('child_process')
 const root = path.dirname(path.dirname(__dirname));
 const args = process.argv.slice(2);
 
-const folders = ['protocol', 'tsc', 'npm', 'sqlite', 'tooling', 'util', 'tsc-tests', 'lsif'];
+const folders = ['protocol', 'tsc', 'npm', 'sqlite', 'tooling', 'tsc-tests', 'lsif'];
 
 for (const folder of folders) {
 	child_process.spawnSync(`npm ${args.join(' ')}`, { cwd: path.join(root, folder), shell: true, stdio: 'inherit' });
--- util/README.md
+++
@@ -1,4 +0,0 @@
-
-# lsif-util
-
-This package has been deprecated. Please use the `lsif` package instead. To validate a LSIF dump use `lsif validate`
