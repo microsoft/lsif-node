998631c968e30fd673b4e8811c3c39310330e31d
Dirk Bäumer <dirkb@microsoft.com>
2024-05-13 16:36:47 +0200

    Add publish pipeline (#169)

--- build/bin/all.js
+++ build/bin/all.js
@@ -12,6 +12,14 @@ const child_process = require('child_process')
 const root = path.dirname(path.dirname(__dirname));
 const args = process.argv.slice(2);
 
+// When we install in a package pipeline then we don't want to call install in
+// the project directories since we need to call install there to ensure proper
+// dependency management.
+if (args[0] === 'install' && process.env['npm_config_root_only'] === 'true') {
+	return;
+}
+
+
 const folders = ['protocol', 'tsc', 'npm', 'sqlite', 'tooling', 'tsc-tests', 'lsif'];
 
 for (const folder of folders) {
